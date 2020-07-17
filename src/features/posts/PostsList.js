21f3ef901fff85d07dc2b9e0e57f797d4598e29c
rochellev <rochellevroberts@utexas.edu>
2020-07-17 13:22:38 -0700

    add posts list component

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+// useSelector so components can read data from redux store
+
+export const PostsList = () => {
+  const posts = useSelector(state => state.posts)
+
+  const renderedPosts = posts.map(post => (
+    <article className="post-excerpt">
+    <h3>{post.title}</h3>
+    <p>{post.content.substring(0, 100)}</p>
+  </article>
+  ))
+  
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  )
+
+}
