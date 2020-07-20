7fa556a72ba78e4abc18654d7169d15da247f210
rochellev <rochellevroberts@utexas.edu>
2020-07-20 11:16:51 -0700

    add postAdded reducer to posts slice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,14 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+// posts slice only knows about the data it's responsible for
+
 const initialState = [
   { id: '1', title: 'First Post!', content: 'Hello!' },
   { id: '2', title: 'Second Post', content: 'More text' }
 ]
 
+// state argument will be array of posts by itself 
+// (only data this slice is aware of)
+
+// Note: createSlice allows for mutable change ---it converts to safe immutable changes with Immer library behind scenes
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {}
+  reducers: {
+    postAdded(state, action){
+      state.push(action.payload)
+    }
+  }
 })
 
 export default postsSlice.reducer
+export const { postAdded } = postsSlice.actions
