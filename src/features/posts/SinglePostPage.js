de0cb5b21e5e658b04fd9b8bd799521463902c2b
rochellev <rochellevroberts@utexas.edu>
2020-07-21 14:48:51 -0700

    add link to edit posts

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
+import {Link} from 'react-router-dom'
 
 // component re-renders when return value from useSelector changes
 
@@ -24,6 +25,9 @@ export const SinglePostPage =({ match }) => {
         <article className="post">
           <h2>{post.title}</h2>
           <p className="post-content">{post.content}</p>
+          <Link to={`/editPost/${post.id}`} className="button">
+            Edit Post
+          </Link>
         </article>
       </section>
     )
