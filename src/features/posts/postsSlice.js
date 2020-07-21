888b835331c44941140c1e3fbee03febe0d9f040
rochellev <rochellevroberts@utexas.edu>
2020-07-21 14:38:24 -0700

    add postUpdated reducer function

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -17,6 +17,14 @@ const postsSlice = createSlice({
   reducers: {
     postAdded(state, action){
       state.push(action.payload)
+    },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.find(post => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
     }
   }
 })
