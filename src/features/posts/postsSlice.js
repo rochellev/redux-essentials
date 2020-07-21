7ab091d96af88a91987ae20d69c193799284d78f
rochellev <rochellevroberts@utexas.edu>
2020-07-21 15:01:36 -0700

    add prepare object so components do not have to worry about what the payload object looks like

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -17,8 +17,19 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded(state, action){
-      state.push(action.payload)
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content
+          }
+        }
+      }
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
