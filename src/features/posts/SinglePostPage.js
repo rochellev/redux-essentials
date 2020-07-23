ab5e6fb3163edc8747bda4cb359632a208b433e7
rochellev <rochellevroberts@utexas.edu>
2020-07-23 09:41:45 -0700

    add post author to single post page component

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {PostAuthor} from './PostAuthor'
 
 // component re-renders when return value from useSelector changes
 
@@ -12,7 +13,6 @@ export const SinglePostPage =({ match }) => {
   const post = useSelector( state =>
     state.posts.find(post => post.id === postId)
     )
-
     if(!post){
       return (
         <section>
@@ -24,6 +24,7 @@ export const SinglePostPage =({ match }) => {
       <section>
         <article className="post">
           <h2>{post.title}</h2>
+          <PostAuthor userId={post.user} />
           <p className="post-content">{post.content}</p>
           <Link to={`/editPost/${post.id}`} className="button">
             Edit Post
